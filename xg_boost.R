b4463ec316bc47dbe83501644b0e40431fd52990
khantsu <amundsonac@gmail.com>
2016-11-26 22:38:45 -0800

    Fixed XGBoost
    XGBoost is now in the standard format

--- xg_boost.R
+++ xg_boost.R
@@ -8,32 +8,40 @@ if (!require('vcd')) {
   require(vcd)
 }
 
-## Create data frame from CSV file
-dat <- read.csv(
-  file = "C:/Users/aaron/OneDrive/Documents/GitHub/DataMineProject/training_r.csv",
+## libraries
+library(caret)
+
+## Read in training data attributes from CSV file
+trainingDat <- read.csv(
+  file = "training_r.csv",
   header = TRUE,
   stringsAsFactors = FALSE
 )
-# dat$Class <- as.factor(dat$Class)
+
+## Read in training data classes from file
+trainingLabels <- read.table("label_training.txt",
+                             header = FALSE)
+
+## Read in testing data attributes from CSV file
+testingDat <- read.csv(
+  file = "testing_r.csv",
+  header = TRUE,
+  stringsAsFactors = FALSE
+)
+
+## Read in testing data classes from file
+testingLabels <- read.table("label_testing.csv",
+                            header = FALSE)
 
 ## For first instance ignore default values
-df <- data.table(dat, keep.rownames = F)
+training <- data.table(trainingDat, keep.rownames = F)
+testing <- data.table(testingDat, keep.rownames = F)
 
-## Split the data into training and testing
-smp_size <- floor(0.75 * nrow(df))
-train_id <- sample(seq_len(nrow(df)), size = smp_size)
-
-training <- df[train_id,]
-output_vector = training[,Class] == 1
-training <- subset(training, select = -Class)
-testing <- df[-train_id,]
-
-# datMat <- sapply(df, as.numeric)
-# trainMat <- sparse.model.matrix(Class~.-1, data = training)
-# testMat <- sparse.model.matrix()
+# Convert to binary values
+training_vector = trainingLabels$V1 == 1
 
 bst <- xgboost(data = data.matrix(training),
-               label = output_vector,
+               label = training_vector,
                max.depth = 6,
                eta = 1,
                nthread = 2,
@@ -41,12 +49,11 @@ bst <- xgboost(data = data.matrix(training),
                verbose = 3,
                objective = "binary:logistic")
 
+xgbClasses <- predict(baggedCart, newdata = data.matrix(testingDat))
+xgbProbs <- predict(baggedCart, newdata = data.matrix(testingDat), type = "prob")
+err <- mean(as.numeric(xgbClasses > 0.5) != testingLabels$V1)
 
-pred <- predict(bst, data.matrix(testing))
-err <- mean(as.numeric(pred > 0.5) != testing$Class)
-
-# importanceRaw <- xgb.importance(@Dimnames[[2]], model = bst, data = data.matrix(training), label = output_vector)
-
+confusionMatrix(data = baggedCartClasses, testingLabels$V1)
 
 names <- dimnames(data.matrix(training))[[2]]
 importance_matrix <- xgb.importance(colnames(training), model = bst)
